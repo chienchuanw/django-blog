45b9d8cf9fd6a9eab7455b69d6b15b330197a7a7
Chien Chuan Wang <chien@chienchuan.com>
2024-05-29 21:34:08 +0800

    Fixed url errors

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="core/home.html"), name="home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("users", include("users.urls")),
