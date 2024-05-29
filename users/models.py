d17c5dd7928d452b7050b3375558979950536d2f
Chien Chuan Wang <chien@chienchuan.com>
2024-05-29 22:15:59 +0800

    Simplified users models.py and redid migrations

--- users/models.py
+++ users/models.py
@@ -5,16 +5,5 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
 
-    groups = models.ManyToManyField(
-        "auth.Group",
-        related_name="userprofile_set",
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        "auth.Permission",
-        related_name="userprofile_set",
-        blank=True,
-    )
-
     def __str__(self):
         return self.username
