b5c023f50491761bbbe70abb5444ccb4ee42d088
Chien Chuan Wang <chien@chienchuan.com>
2024-06-01 01:08:11 +0800

    Added PostCreateView with related form and path

--- posts/urls.py
+++ posts/urls.py
@@ -6,4 +6,5 @@ app_name = "posts"
 urlpatterns = [
     # path("", PostListView.as_view(), name="list"),
     path("", post_list, name="list"),
+    path("create/", PostCreateView.as_view(), name="create"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from .models import *
+from .forms import *
 
 
 class PostListView(ListView):
@@ -8,3 +13,11 @@ class PostListView(ListView):
 
 def post_list(request):
     return render(request, "posts/list.html")
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/create.html"
+    login_url = reverse_lazy("users:login")
+    success_url = reverse_lazy("posts:list")
