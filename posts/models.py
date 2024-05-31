567ab64dd71610bb7a398f58a856029025ba4fa8
Chien Chuan Wang <chien@chienchuan.com>
2024-05-31 23:29:56 +0800

    Created class Post any yet to make migration

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import *
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    content = models.TextField(blank=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
