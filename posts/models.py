84cb136c5b4356f6868992391e389896acd996e8
Chien Chuan Wang <chien@chienchuan.com>
2024-06-01 21:52:14 +0800

    Modified published_at value and added publish function.

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import *
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -8,8 +9,12 @@ class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     tag = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    published_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
