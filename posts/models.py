f008c6880083092dd20770d107f4a2362cd30bbe
Chien Chuan Wang <chien@chienchuan.com>
2024-07-03 11:51:52 +0800

    Added model Tag; Replaced tag with tags in model Post and registered it as a ManyToManyField

--- posts/models.py
+++ posts/models.py
@@ -3,11 +3,18 @@ from users.models import CustomUser
 from django.utils import timezone
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255, blank=False)
     content = models.TextField(blank=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    tag = models.CharField(max_length=100)
+    tags = models.ManyToManyField(Tag, related_name="posts")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(null=True, blank=True)
