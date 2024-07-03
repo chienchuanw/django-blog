e1300e2d1850788703691a715150f1e197955ca1
Chien Chuan Wang <chien@chienchuan.com>
2024-07-03 13:41:06 +0800

    Added PostDetailView

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostCreateView, PostUpdateView, PostListView
+from .views import PostCreateView, PostUpdateView, PostListView, PostDetailView
 
 app_name = "posts"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", PostListView.as_view(), name="list"),
     path("create/", PostCreateView.as_view(), name="create"),
     path("<int:pk>/update", PostUpdateView.as_view(), name="update"),
+    path("<int:pk>/", PostDetailView.as_view(), name="detail"),
 ]
