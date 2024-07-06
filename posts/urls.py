f5870e415c1bd6139119297a41c49f3323a7667d
chienchuanw <chienchuanwww@gmail.com>
2024-07-06 20:27:28 +0800

    modified #10: Substitued pk with slug for PostDetailView and PostUpdateView paths

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,6 @@ app_name = "posts"
 urlpatterns = [
     path("", PostListView.as_view(), name="list"),
     path("create/", PostCreateView.as_view(), name="create"),
-    path("<int:pk>/update", PostUpdateView.as_view(), name="update"),
-    path("<int:pk>/", PostDetailView.as_view(), name="detail"),
+    path("<slug:slug>/update/", PostUpdateView.as_view(), name="update"),
+    path("<slug:slug>/", PostDetailView.as_view(), name="detail"),
 ]
