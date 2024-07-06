6d37d5dd119cf712fef950187dd107290e24566f
chienchuanw <chienchuanwww@gmail.com>
2024-07-06 20:10:32 +0800

    Modified #10: Added 'slug' in class 'Post' and allowed it prepopulated based on post.title

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,11 @@ from django.utils import timezone
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Tag name")
+
+    class Meta:
+        verbose_name = "Post Tag"
+        verbose_name_plural = "Post Tags"
 
     def __str__(self) -> str:
         return self.name
@@ -15,9 +19,14 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, related_name="posts")
+    slug = models.SlugField(blank=True, null=True, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
 
     def __str__(self):
         tags = ", ".join(f"#{tag.name}" for tag in self.tags.all())
