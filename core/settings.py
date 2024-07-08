d25527340ff462d8d4f856019136636760052d2d
chienchuanw <chienchuanwww@gmail.com>
2024-07-08 22:43:09 +0800

    modified #14: Configured media path in settings.py

--- core/settings.py
+++ core/settings.py
@@ -120,5 +120,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
