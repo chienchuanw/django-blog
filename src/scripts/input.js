22f823c4831d69e88088b1e1ca2f7a48c25c7307
chienchuanw <chienchuanwww@gmail.com>
2024-07-10 23:54:35 +0800

    modified #14: Allowed user to upload images and get url link in the textarea afterwards

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,4 +1,34 @@
-import "htmx.org"
-import Alpine from "alpinejs"
+import "htmx.org";
+import Alpine from "alpinejs";
 
-Alpine.start()
+document.addEventListener("alpine:init", () => {
+  Alpine.data("uploadImage", () => ({
+    images: [],
+    uploadImage(evt) {
+      const file = evt.target.files[0];
+      const formData = new FormData();
+      formData.append("image", file);
+
+      fetch("/posts/image/upload/", {
+        method: "POST",
+        body: formData,
+        headers: {
+          "X-CSRFToken": document.querySelector("input[name=csrfmiddlewaretoken]").value
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.url) {
+            this.images.push({ name: file.name, url: data.url });
+          } else {
+            alert("Image upload failed");
+          }
+        })
+        .catch(error => {
+          console.error("Error", error);
+        });
+    }
+  }));
+});
+
+Alpine.start();
